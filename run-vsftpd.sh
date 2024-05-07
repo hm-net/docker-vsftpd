4a8eddd5b44b3e2fe49f83209cfdd899d8323635
HyeonJunOh <dhguswns23@naver.com>
2024-05-07 19:39:44 +0900

    Chore: change id modification in sh

--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -10,7 +10,7 @@ if [ "$FTP_PASS" = "**Random**" ]; then
     export FTP_PASS=`cat /dev/urandom | tr -dc A-Z-a-z-0-9 | head -c${1:-16}`
 fi
 
-groupdel www-data
+groupdel tape
 groupmod -g 33 ftp
 
 # Do not log to STDOUT by default:
